1700511b8635113b7abfb01543c18d4b9f8d8492
Philip Schlump <pschlump@gmail.com>
2019-01-09 06:33:25 -0700

    1st pass - not complete

--- server/midlib/acb1/acb1.go
+++ server/midlib/acb1/acb1.go
@@ -33,6 +33,7 @@ func init() {
 	}
 	mid.RegInitItem3("Acb1", CreateEmpty, `{
 		"Paths":        	 { "type":["string","filepath"], "isarray":true, "required":true },
+		"AuthKey":  	     { "type":[ "string" ], "required":false, "default":"" },
 		"RedisPrefix":  	 { "type":[ "string" ], "required":false, "default":"dip:" },
 		"InputPath":  	     { "type":[ "string" ], "required":false, "default":"./image" },
 		"OutputPath":  	     { "type":[ "string" ], "required":false, "default":"./qr-final" },
@@ -53,6 +54,8 @@ func init() {
 // Used by /api/acb1/generateQrFor
 // 		OutputURL -	/qr-final
 // 		OutputPath - path for generation of .png/.svg QR Codes
+//
+// AuthKey - key used to auth calls to this.
 //
 // Not Used Yet -- or -- will be removed from old code:
 //		"RedisPrefix":  	 { "type":[ "string" ], "required":false, "default":"dip:" },
@@ -88,6 +91,7 @@ var _ mid.GoFTLMiddleWare = (*Acb1Type)(nil)
 type Acb1Type struct {
 	Next           http.Handler                //
 	Paths          []string                    //
+	AuthKey        string                      // (acb)
 	RedisPrefix    string                      //
 	InputPath      string                      //
 	OutputPath     string                      //
@@ -100,6 +104,7 @@ type Acb1Type struct {
 	RedisID        string                      // ID to increment for temp file names
 	SingedOnceAddr string                      // Address of loaded proxy contract
 	AppID          string                      // ID of this app
+	validEvent     map[string]bool             // list of valid events for items (acb)
 	LineNo         int                         //
 	gCfg           *cfg.ServerGlobalConfigType //
 }
@@ -139,8 +144,7 @@ type metaDocumentType struct {
 }
 
 type dispatchType struct {
-	hdlr func(rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request)
-	// func DecryptData(hdlr *AesSrpType, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, SandBoxPrefix, Password, tEmail, tSalt string, encData *sjcl.SJCL_DataStruct, tIter, tKeySize int, tKey string, Session map[string]interface{}, tt string, debugFlag1, debugFlag2 bool) (plaintext, key []byte, err error) {
+	handlerFunc func(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request)
 }
 
 var dispatch map[string]dispatchType
@@ -149,12 +153,156 @@ func init() {
 	dispatch = make(map[string]dispatchType)
 
 	dispatch["/api/acb1/test1"] = dispatchType{
-		hdlr: func(rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request) {
+		handlerFunc: func(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request) {
 			fmt.Printf("test1 called\n")
 			fmt.Fprintf(os.Stderr, "test1 called\n")
 		},
 	}
+	dispatch["/api/acb1/track_add"] = dispatchType{
+		handlerFunc: trackAdd,
+	}
 
+}
+
+type bulkDataRow struct {
+	Tag   string `json:"Tag"`    // RFIF etc. (unique)
+	SubId string `json:"Sub_id"` // Used with Site_id to pull out Tag
+	Event string `json:"Event"`  // One of standard set of vevents - will be validated.
+	Data  string `json:"Data"`   // Additional Data in JSON format
+	Date  string `json:"Date"`   // Date/Time ISO format date/time stamp.
+}
+type bulkDataType struct {
+	SiteId string `json:"Site_id"`
+	Auth   string `json:"Auth"` // AuthKey for this.
+	Row    []bulkDataRow
+}
+
+// Set of results - per tag id
+type bulkRvListType struct {
+	Tag        string `json:"Tag"` // RFIF etc. (unique)
+	SiteId     string `json:"Site_id"`
+	SubId      string `json:"Sub_id"`     // Used with SiteId to pull out Tag
+	ItemStatus string `json:"ItemStatus"` // Error for this
+	Msg        string `json:"Msg"`        // Used with SiteId to pull out Tag
+}
+type bulkRvType struct {
+	Status string           `json:"status"` // status of success or "partial", or "error"
+	Msg    string           `json:"msg"`    // msg - if not "", then all failed.
+	Detail []bulkRvListType `json:"detail"`
+}
+
+func (hdlr *Acb1Type) SetupValidEvents() {
+	if hdlr.validEvent == nil {
+		hdlr.validEvent = make(map[string]bool)
+		hdlr.validEvent["Init"] = true
+		hdlr.validEvent["Update"] = true
+	}
+}
+
+func (hdlr *Acb1Type) InsertTrackAdd(tag string) error {
+	stmt := "insert into \"track_animal\" ( \"tag\" ) values ( $1 )"
+	_, err := hdlr.gCfg.Pg_client.Db.Exec(stmt, tag)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func trackAdd(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request) {
+	fmt.Printf("trackAdd called\n")
+	fmt.Fprintf(os.Stderr, "trackAdd called\n")
+
+	ps := &rw.Ps
+
+	bulk := ps.ByNameDflt("bulk", "")
+	var bulkData bulkDataType
+	var err error
+
+	if bulk == "" {
+		err = json.Unmarshal([]byte(bulk), &bulkData)
+	} else {
+		err = nil
+		bulkData.Auth = ps.ByNameDflt("auth", "")
+		bulkData.SiteId = ps.ByNameDflt("Site_id", "")
+		bulkData.Row = append(bulkData.Row, bulkDataRow{
+			Tag:   ps.ByNameDflt("Tag", ""),
+			SubId: ps.ByNameDflt("Sub_id", ""),
+			Event: ps.ByNameDflt("Event", ""),
+			Data:  ps.ByNameDflt("Data", ""),
+			Date:  ps.ByNameDflt("Date", ""),
+		})
+	}
+
+	if err != nil && hdlr.AuthKey != "" && bulkData.Auth == hdlr.AuthKey {
+		err = fmt.Errorf("Invalid auth key")
+	}
+
+	var rv bulkRvType
+	statusVal := "success"
+
+	hdlr.SetupValidEvents()
+	for _, rr := range bulkData.Row {
+		if _, ok := hdlr.validEvent[rr.Event]; !ok {
+			rv.Detail = append(rv.Detail, bulkRvListType{
+				Tag:        rr.Tag,
+				SiteId:     bulkData.SiteId,
+				SubId:      rr.SubId,
+				ItemStatus: "error",
+				Msg:        "Invalid Event Type",
+			})
+			statusVal = "partial"
+			err = nil
+		} else {
+			rv.Detail = append(rv.Detail, bulkRvListType{
+				Tag:        rr.Tag,
+				SiteId:     bulkData.SiteId,
+				SubId:      rr.SubId,
+				ItemStatus: "sucess",
+			})
+		}
+	}
+
+	for ii, rr := range bulkData.Row {
+		if rv.Detail[ii].ItemStatus == "success" {
+			if rr.Tag == "" && rr.SubId != "" {
+				// xyzzy - pull out Tag id or error -- If error set ItemStatus to...
+				_ = ii
+			}
+		}
+		if rv.Detail[ii].ItemStatus == "success" {
+			err = hdlr.InsertTrackAdd(rr.Tag) // xyzzy - other params to pass!
+			if err != nil {
+				statusVal = "partial"
+				rv.Detail[ii].ItemStatus = "error"
+				rv.Detail[ii].Msg = fmt.Sprintf("%s", err)
+				err = nil
+			}
+		}
+	}
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%sError[%s] AT: %s%s\n", MiscLib.ColorRed, err, godebug.LF(), MiscLib.ColorReset)
+		fmt.Fprintf(os.Stdout, "%sError[%s] AT: %s%s\n", MiscLib.ColorRed, err, godebug.LF(), MiscLib.ColorReset)
+
+		fmt.Fprintf(www, "%s", godebug.SVarI(bulkRvType{
+			Status: "failed",
+			Msg:    "error - falied to supply needed data for processing.",
+		}))
+		return
+	}
+
+	if statusVal != "success" {
+		rv.Status = statusVal
+		fmt.Fprintf(os.Stderr, "%sPartial Error[%s] AT: %s%s\n", MiscLib.ColorRed, err, godebug.SVarI(rv), MiscLib.ColorReset)
+		fmt.Fprintf(os.Stdout, "%sPartial Error[%s] AT: %s%s\n", MiscLib.ColorRed, err, godebug.SVarI(rv), MiscLib.ColorReset)
+
+		fmt.Fprintf(www, "%s", godebug.SVarI(rv))
+		return
+	}
+
+	fmt.Fprintf(www, "%s", godebug.SVarI(bulkRvType{
+		Status: "success",
+	}))
 }
 
 func (hdlr *Acb1Type) ServeHTTP(www http.ResponseWriter, req *http.Request) {
@@ -172,6 +320,7 @@ func (hdlr *Acb1Type) ServeHTTP(www http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(os.Stdout, "%sAT: %s%s\n", MiscLib.ColorGreen, godebug.LF(), MiscLib.ColorReset)
 
 			if true {
+
 				hdlr.SetupServer()
 
 				fx, ok := dispatch[req.URL.Path]
@@ -182,7 +331,7 @@ func (hdlr *Acb1Type) ServeHTTP(www http.ResponseWriter, req *http.Request) {
 					fmt.Fprintf(www, "{\"status\":\"not-implemented-yet\"}")
 					return
 				}
-				fx.hdlr(rw, www, req)
+				fx.handlerFunc(hdlr, rw, www, req)
 				return
 
 			} else {
