c879e3621025fc3ffcbba97fea43383492e7d93d
Philip Schlump <pschlump@gmail.com>
2019-01-13 07:23:33 -0700

    Update

--- server/midlib/acb1/ddl.sql
+++ server/midlib/acb1/ddl.sql
@@ -22,6 +22,11 @@ CREATE TABLE "v1_trackAdd" (
 , "created" 			timestamp default current_timestamp not null 					
 );
 
+-- create unique index "v1_trackAdd_u1" on "v1_trackAdd" ( "premis_id", "premis_animal" );
+
+
+
+
 CREATE SEQUENCE v1_qr_avail_seq
 INCREMENT 1
 MINVALUE 1
@@ -55,66 +60,13 @@ insert into "v1_avail_qr" ( "file_name", "url_path", "qr_id", "qr_enc_id") value
 ,	( 'qr004.png', 'http://t5432z/q/3.png', '4', '4' )
 ;
 
-/*
-	fmt.Printf("generateQrFor called\n")
-	fmt.Fprintf(os.Stderr, "generateQrFor called\n")
 
-	// TODO
-	// pick out a QR to use
-	stmt := "select * from \"v1_avail_qr\" where \"state\" = 'avail'"
-	// update the QR row
-	stmt = "update \"v1_avail_qr\" where \"state\" = 'avail'"
-	// do get/post request to update where to on QR code
-
-	fmt.Fprintf(www, `{"status":"success"}`)
-
-func generateQrFor(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
-	fmt.Printf("generateQrFor called\n")
-	fmt.Fprintf(os.Stderr, "generateQrFor called\n")
-
-	stmt := "select v1_next_avail_qr as \"x\""
-
-	// TODO - call function, return x
-
-	fmt.Fprintf(www, `{"status":"success"}`)
-}
-
-func getTagId(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
-	fmt.Printf("getTagId called\n")
-	fmt.Fprintf(os.Stderr, "getTagId called\n")
-
-	// TODO - convert a premis_id/sub_id -> tag id and return
-
-	fmt.Fprintf(www, `{"status":"success"}`)
-}
-
-func getInfo(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
-	fmt.Printf("getInfo called\n")
-	fmt.Fprintf(os.Stderr, "getInfo called\n")
-
-	// TODO - get all the info on a cow
-
-	fmt.Fprintf(www, `{"status":"success"}`)
-}
-
-func convToJson(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
-	fmt.Printf("convToJson called\n")
-	fmt.Fprintf(os.Stderr, "convToJson called\n")
-
-	// TODO -- get all the info on a cow and convert to JSON and return
-
-	fmt.Fprintf(www, `{"status":"success"}`)
-}
-
-func chainHash(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
-	fmt.Printf("chainHash called\n")
-	fmt.Fprintf(os.Stderr, "chainHash called\n")
-
-*/
-
+-- pick out a QR to use
+-- update the QR row
+-- do get/post request to update where to on QR code
 drop FUNCTION v1_next_avail_qr ;
-CREATE OR REPLACE FUNCTION v1_next_avail_qr 
-	RETURNS varchar AS $$
+CREATE OR REPLACE FUNCTION v1_next_avail_qr ()
+	RETURNS varchar AS 
 $body$
 DECLARE
     l_id char varying(40);
@@ -137,7 +89,7 @@ BEGIN
 		limit 1
 		;
 	if not found then
-		l_fail = true
+		l_fail = true;
 		l_data = '{"status":"error","code":"100","msg":"unable to generate QR code"}';
 	end if;
 
@@ -155,9 +107,136 @@ BEGIN
 			||'}';
 	end if;
 
+	RETURN l_data;
 END;
 $body$
 LANGUAGE plpgsql;
+
+-- select v1_next_avail_qr() as "x";
+
+
+
+
+
+
+-- TODO - conv from site_id/sub_id -> tag
+drop FUNCTION v1_getTagId p_site_id varchar, p_sub_id varchar );
+CREATE OR REPLACE FUNCTION v1_getTagId ( p_site_id varchar, p_sub_id varchar )
+	RETURNS varchar AS 
+$body$
+DECLARE
+    l_id 				char varying(40);
+	l_data				varchar (800);
+	l_tag				varchar (80);
+	l_fail				bool;
+BEGIN
+
+	l_fail = false;
+	l_data = '{"status":"success"}';
+
+	select "tag"
+		into l_tag
+		from "v1_trackAdd"
+		where "premis_id" = p_site_id
+		  and "premis_animal" = p_sub_id
+		limit 1
+		;
+	if not found then
+		l_fail = true;
+		l_data = '{"status":"error","code":"101","msg":"unable to convert to tag"}';
+	end if;
+
+	if not l_fail then
+		l_data = '{"status":"success"'
+				||'"tag":'||to_json(l_tag)
+			||'}';
+	end if;
+
+	RETURN l_data;
+END;
+$body$
+LANGUAGE plpgsql;
+
+
+-- update "v1_trackAdd" set "premis_id" = '500', "premis_animal" = '3' where "tag" = '34000000000001';
+-- update "v1_trackAdd" set "premis_id" = '500', "premis_animal" = '4' where "tag" = '34000000000002';
+-- select v1_getTagId ( '500', '3' );
+
+
+
+/*
+func getInfo(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
+	fmt.Printf("getInfo called\n")
+	fmt.Fprintf(os.Stderr, "getInfo called\n")
+
+
+	fmt.Fprintf(www, `{"status":"success"}`)
+}
+*/
+-- pull back all info on a cow. (Differnt from JSON below?)
+drop FUNCTION v1_getInfo ;
+CREATE OR REPLACE FUNCTION v1_getInfo ( p_tag varchar )
+	RETURNS varchar AS 
+$body$
+DECLARE
+    l_id 				char varying(40);
+	l_data				varchar (8000);
+	l_tag				varchar (80);
+	-- xyzzy
+	l_fail				bool;
+BEGIN
+
+	l_fail = false;
+	l_data = '{"status":"success"}';
+
+	// TODO
+	-- xyzzy
+
+	RETURN l_data;
+END;
+$body$
+LANGUAGE plpgsql;
+
+
+
+
+
+/*
+func convToJson(hdlr *Acb1Type, rw *goftlmux.MidBuffer, www http.ResponseWriter, req *http.Request, mdata map[string]string) {
+	fmt.Printf("convToJson called\n")
+	fmt.Fprintf(os.Stderr, "convToJson called\n")
+
+	// TODO -- get all the info on a cow and convert to JSON and return
+
+	fmt.Fprintf(www, `{"status":"success"}`)
+}
+*/
+drop FUNCTION v1_convToJson ;
+CREATE OR REPLACE FUNCTION v1_convToJson ()
+	RETURNS varchar AS 
+$body$
+DECLARE
+    l_id 				char varying(40);
+	l_data				varchar (800);
+	l_fail				bool;
+BEGIN
+
+	l_fail = false;
+	l_data = '{"status":"success"}';
+
+	// TODO
+
+	RETURN l_data;
+END;
+$body$
+LANGUAGE plpgsql;
+
+
+
+
+
+
+
 
 
 
