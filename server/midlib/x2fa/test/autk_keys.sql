3101fd5467b47209f14855536b9342a71ef88974
Philip Schlump <pschlump@gmail.com>
2019-01-31 10:39:14 -0700

    Update

---
+++ server/midlib/x2fa/test/autk_keys.sql
@@ -0,0 +1,7 @@
+select t1."user_id", t1."auth_token", t2."username"
+from "t_auth_token" as t1 join "t_user" as t2 on t1."user_id" = t2."id"
+order by 3, 1, 2;
+
+-- select t1."user_id", t1."auth_token", t2."username"
+-- from "t_auth_token" as t1 left outer join "t_user" as t2 on t1."user_id" = t2."id"
+-- order by 3, 1, 2;
---
+++ server/midlib/x2fa/test/list-user_id.sql
@@ -0,0 +1,1 @@
+select "id" as "user_id", "username" from "t_user" order by 2;
